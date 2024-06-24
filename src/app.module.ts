95c44165021d8272efe324a5025f8488da8a390e
San Valen <osoriovalentin5@gmail.com>
2024-06-24 17:51:02 -0500

    chore: Add HealthCheckModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [PaymentsModule],
-  controllers: [],
-  providers: [],
+  imports: [PaymentsModule, HealthCheckModule],
 })
 export class AppModule {}
